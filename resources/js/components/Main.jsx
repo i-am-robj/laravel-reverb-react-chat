007063dc8b5ce62d2272bb3895c8c241d8a96ae0
RobJ <rob@robj.co.uk>
2024-04-02 17:55:19 +0100

    Installed reverb

--- resources/js/components/Main.jsx
+++ resources/js/components/Main.jsx
@@ -5,7 +5,8 @@ import '../../css/app.css';
 import ChatBox from "./ChatBox.jsx";
 
 if (document.getElementById('main')) {
-    const rootUrl = "http://127.0.0.1:8000";
+    /* take the rootURL from the VITE_APP_URL environment variable */
+    const rootUrl = import.meta.env.VITE_APP_URL;
 
     ReactDOM.createRoot(document.getElementById('main')).render(
         <React.StrictMode>
